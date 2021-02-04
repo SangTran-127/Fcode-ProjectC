fb67c3f197ab69f59edce35efaba00acba8f1845
Thu Trang Le <lethutrang1722@gmail.com>
2021-02-04 21:38:40 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,18 +1,4 @@
-#include <stdio.h>
-
-int multiplyTwoIntegers(int number1, int number2);
-
-int sumNumber(int num1, int num2);
-
-int main() {
-    printf("hello");
-    printf("number1 + number2 = %d", sumNumber(num1, num2));
-    printf("number1 * number2 = %d", multiplyTwoIntegers(num1, num2));
-    return 0;
-}
-int multiplyTwoIntegers(int number1, int number2){
-    return number1 * number2;
-}
-int sumNumber(int num1, int num2) {
-    return num1 + num2;
+int differenceTwoIntergers(int number1, int number2)
+{
+return number1 - number2;
 }

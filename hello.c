67dea1511c9116a8b60aa5d66b52bfc31ccceab2
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-02-04 21:22:12 +0700

    function sum

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+int sumNumber(int num1, int num2) {
+    return num1 + num2;
+}
 int main() {
     printf("hello");
+    printf("number1 + number2 = %d", sumNumber(num1, num2));
     return 0;
-    hello
 }

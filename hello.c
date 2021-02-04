068a28445b96c3001714e9ce89fb9f52f764fce1
Trần Quang Sáng <tranquangsang.127@gmail.com>
2021-02-04 21:43:04 +0700

    Revert "Update hello.c"
    This reverts commit fb67c3f197ab69f59edce35efaba00acba8f1845.

--- hello.c
+++ hello.c
@@ -1,4 +1,18 @@
-int differenceTwoIntergers(int number1, int number2)
-{
-return number1 - number2;
+#include <stdio.h>
+
+int multiplyTwoIntegers(int number1, int number2);
+
+int sumNumber(int num1, int num2);
+
+int main() {
+    printf("hello");
+    printf("number1 + number2 = %d", sumNumber(num1, num2));
+    printf("number1 * number2 = %d", multiplyTwoIntegers(num1, num2));
+    return 0;
+}
+int multiplyTwoIntegers(int number1, int number2){
+    return number1 * number2;
+}
+int sumNumber(int num1, int num2) {
+    return num1 + num2;
 }

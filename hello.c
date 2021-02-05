52bb06fdb467d8130f36a57720203776655855a5
zhangliejunjie <45983207+zhangliejunjie@users.noreply.github.com>
2021-02-05 08:48:05 +0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -8,6 +8,7 @@ int main() {
     printf("hello");
     printf("number1 + number2 = %d", sumNumber(num1, num2));
     printf("number1 * number2 = %d", multiplyTwoIntegers(num1, num2));
+    printf("number1 * number2 = %d", differenceTwoIntergers(num1, num2));
     return 0;
 }
 int multiplyTwoIntegers(int number1, int number2){
@@ -18,7 +19,4 @@ int sumNumber(int num1, int num2) {
 }
 int differenceTwoIntergers(int number1, int number2){
     return number1 - number2;
-}
-void reheasal(void){
-    printf("\nHello World");
 }
